05c1e0c0a344d1f84621d8ae3a34f18574b79f40
ademten82 <ademten7@gmail.com>
2021-12-03 15:39:32 +0100

    updated NotFound page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,5 +2,5 @@ import React from "react";
 import "./Footer.css";
 
 export default function Footer() {
-  return <footer className="Footer">Footer</footer>;
+  return <footer className="Footer">&copy; December 2021</footer>;
 }
--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,10 +1,16 @@
-import './NotFound.css'
-import React from 'react'
+import "./NotFound.css";
+import React from "react";
+import bear from "../../images/bear.gif";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
-    return (
-        <div>
-            NotFound
-        </div>
-    )
+  return (
+    <div>
+      <h1>Sorry, Page not Found</h1>
+      <img src={bear} alt="bear-gif" />
+      <Link to="/">
+        <button className="go-home">Go Home</button>
+      </Link>
+    </div>
+  );
 }
